64dfc195df9b1d44c5f22f7ec94e31025e0c4e4f
Philzace1 <unknownumeh@gmail.com>
2020-06-19 13:32:49 +0100

    deployed and completed

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -9,19 +9,7 @@ app.use(express.static("dist"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
-app.get("/users", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      firstName: "Akuagwu",
-      lastName: "Philemon",
-      email: "ace@gmail.com",
-    },
-    { id: 2, firstName: "Emeka", lastName: "Joy", email: "joy@gmail.com" },
-    { id: 3, firstName: "Mordi", lastName: "Joy", email: "some@gmail.com" },
-    { id: 4, firstName: "Danni", lastName: "Dave", email: "dave@gmail.com" },
-  ]);
-});
+
 app.listen(PORT, (err) => {
   if (err) {
     // eslint-disable-next-line no-console
--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,7 @@
 export default function getBaseUrl() {
   return getQueryStringParameterByName("useMockApi")
     ? "http://localhost:3001/"
-    : "/";
+    : "https://coryphilzapi.herokuapp.com/";
 }
 
 function getQueryStringParameterByName(name, url) {
